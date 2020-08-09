fec4d88ca92e0f34eaa12a5395f4b5ae229f849a
unknown <1654388696@qq.com>
2020-08-09 16:52:10 +0800

    compute mean std

--- utils.py
+++ utils.py
@@ -1,10 +1,42 @@
 import cv2 as cv
-from pycocotools.coco import COCO
-
+# from pycocotools.coco import COCO
+from coco import COCO
+import json
 import config
-
+import glob
 import numpy as np
 from matplotlib import pyplot as plt
+from tqdm import tqdm
+from pathlib import Path
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, init_shape, meter_info='AverageMeter'):
+        self.shape = init_shape
+        self.meter_info = meter_info
+        self.val = np.zeros(shape=self.shape)
+        self.avg = np.zeros(shape=self.shape)
+        self.sum = np.zeros(shape=self.shape)
+        self.count = 0
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        assert list(val.shape) == self.shape, f"{list(val.shape)}-{self.shape} not matched!"
+
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __repr__(self):
+        return f"{self.meter_info}: {self.avg}"
 
 
 def compute_iou(box1, box2, wh=False):
@@ -16,7 +48,7 @@ def compute_iou(box1, box2, wh=False):
     Return:
         iou: iou of box1 and box2.
     """
-    if wh == False:
+    if not wh:
         xmin1, ymin1, xmax1, ymax1 = box1
         xmin2, ymin2, xmax2, ymax2 = box2
     else:
@@ -25,13 +57,13 @@ def compute_iou(box1, box2, wh=False):
         xmin2, ymin2 = int(box2[0] - box2[2] / 2.0), int(box2[1] - box2[3] / 2.0)
         xmax2, ymax2 = int(box2[0] + box2[2] / 2.0), int(box2[1] + box2[3] / 2.0)
 
-    ## 获取矩形框交集对应的左上角和右下角的坐标（intersection）
+    # 获取矩形框交集对应的左上角和右下角的坐标（intersection）
     xx1 = np.max([xmin1, xmin2])
     yy1 = np.max([ymin1, ymin2])
     xx2 = np.min([xmax1, xmax2])
     yy2 = np.min([ymax1, ymax2])
 
-    ## 计算两个矩形框面积
+    # 计算两个矩形框面积
     area1 = (xmax1 - xmin1) * (ymax1 - ymin1)
     area2 = (xmax2 - xmin2) * (ymax2 - ymin2)
 
@@ -41,7 +73,13 @@ def compute_iou(box1, box2, wh=False):
     return iou
 
 
-def main():
+def coco_merge(coco_a, coco_b):
+    a = json.load(open(coco_a, 'r', encoding='utf-8'))
+    b = json.load(open(coco_b, 'r', encoding='utf-8'))
+    assert a['cata']
+
+
+def test_iou():
     x_ray = f'{config.root_dir}/coco/annotations/fold0/all.json'
     coco = COCO(x_ray)
 
@@ -66,5 +104,25 @@ def main():
     print(iou)
 
 
+def compute_mean_std():
+
+    all_images = glob.glob('E:/iFLYTEK/x_ray/train/all_train' + '/**/*.jpg', recursive=True)
+    mean_meter = AverageMeter([3], meter_info='image RGB mean average meter')
+    std_meter = AverageMeter([3], meter_info='image RGB std average meter')
+    pbar = tqdm(all_images, total=len(all_images))
+    for image_path in pbar:
+        pbar.set_description(image_path)
+        p = Path(image_path)
+        image = cv.imread(p.as_posix())
+        mean = np.mean(image, axis=(0, 1))
+        std = np.std(image, axis=(0, 1))
+
+        mean_meter.update(mean)
+        std_meter.update(std)
+
+    print(mean_meter)  # [191.85634426 207.56213923 202.32689929]
+    print(std_meter)  # [59.52199661 52.59761332 66.76140793]
+
+
 if __name__ == '__main__':
-    main()
+    compute_mean_std()

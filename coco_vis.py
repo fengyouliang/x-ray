05fc39f4ae19a23d9c9fd25f3fd6bde77d4241a3
unknown <1654388696@qq.com>
2020-08-09 21:58:12 +0800

    add aug vis

--- coco_vis.py
+++ coco_vis.py
@@ -3,9 +3,10 @@ import numpy as np
 # from pycocotools.coco import COCO
 from coco import COCO
 import config
+from albumentations import *
+from pathlib import Path
 
-
-def my_coco_vis(coco, cats, image_id=None, rotate=None):
+def my_coco_vis(coco, cats, image_id=None, augs=None, rotate=None):
     assert rotate in [None, 0, 1, 2]
     if image_id is None:
         imgIds = coco.getImgIds()
@@ -13,9 +14,16 @@ def my_coco_vis(coco, cats, image_id=None, rotate=None):
     assert image_id in coco.getImgIds()
     coco_img = coco.loadImgs(ids=image_id)[0]
     img_id = coco_img['id']
-    image_path = f"{config.image_root_path}/{coco_img['filename']}"
+    image_path = f"{config.image_root_path}/{coco_img['file_name']}"
+    assert Path(image_path).is_file(), image_path
+
     image = cv.imread(image_path)
     assert image is not None, f'{image_path}'
+    ori_image = image.copy()
+
+    if augs is not None:
+        for aug in augs:
+            image = aug(image=image)['image']
 
     annIds = coco.getAnnIds(imgIds=img_id, iscrowd=None)
     anns = coco.loadAnns(annIds)
@@ -30,17 +38,32 @@ def my_coco_vis(coco, cats, image_id=None, rotate=None):
             color = [0, 0, 0]
         x, y, w, h = list(map(int, ann['bbox']))
 
+        cv.rectangle(ori_image, (x, y), (x + w, y + h), color=color, thickness=2)
         cv.rectangle(image, (x, y), (x + w, y + h), color=color, thickness=2)
+
+        cv.putText(ori_image, label_name, (x, y), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=0.7, thickness=2, color=color)
         cv.putText(image, label_name, (x, y), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=0.7, thickness=2, color=color)
+
     if rotate is not None:
+        ori_image = cv.rotate(ori_image, rotate)
         image = cv.rotate(image, rotate)
-    cv.imshow(coco_img['filename'], image)
+
+    h, w = ori_image.shape[:2]
+    if h > w:
+        concat_image = np.concatenate([ori_image, image], axis=1)  # w
+    else:
+        concat_image = np.concatenate([ori_image, image], axis=0)  # h
+
+    window_name = coco_img['file_name'] + '  ori_aug'
+    cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
+    cv.imshow(window_name, concat_image)
     cv.waitKey()
     cv.destroyAllWindows()
 
 
 def main():
-    x_ray = f'E:/iFLYTEK/x_ray/train\coco/annotations/all.json'
+    x_ray = f'E:/iFLYTEK/x_ray/train\coco/annotations/fold0/all.json'
+    # x_ray = f'E:/iFLYTEK/x_ray/train\coco/annotations/all.json'
     # x_ray = f'E:/download/iFYTEK/video_ad/train/train_seg.json'
 
     coco = COCO(x_ray)
@@ -50,8 +73,13 @@ def main():
     #     v['color'] = (np.random.random((1, 3)) * 255).astype(np.int).tolist()[0]
     #     v['color'] = config.colors[idx % len(config.colors)]
 
+    aug_bc = RandomBrightnessContrast(brightness_limit=0.5, contrast_limit=0.5, p=1)
+    aug_clahe = CLAHE(clip_limit=4.0, tile_grid_size=(8, 8), p=1)
+
+    augs = [aug_clahe]
+
     while True:
-        my_coco_vis(coco, cats=cats, image_id=100001, rotate=None)
+        my_coco_vis(coco, cats=cats, image_id=None, augs=augs, rotate=None)
 
 
 if __name__ == '__main__':
